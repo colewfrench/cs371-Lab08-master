ce563fc4b1280f4a0954a2e4c5ab1b0477b7459e
Lucy Davidson <davidsol20@up.edu>
2017-03-30 18:45:19 -0700

    Checkpoint 2

--- app/src/main/java/edu/up/cs371/soccer_application/SoccerDatabase.java
+++ app/src/main/java/edu/up/cs371/soccer_application/SoccerDatabase.java
@@ -201,4 +201,9 @@ public class SoccerDatabase implements SoccerDB {
         return new HashSet<String>();
 	}
 
+    private boolean searchPlayer(String first, String last)
+    {
+        return (soccerPlayers.containsKey(first + "|" + last));
+    }
+
 }
